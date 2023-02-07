b597aaa97ad65aaf9548137af623fcfd2791578a
R24-Philipp <pt@rueckwand24.com>
2023-02-07 10:00:14 +0100

    inital

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import time
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+class Testwebpage:
+    load_dotenv()
+
+    def test_url(self):
+        driver.get(os.getenv("BASE_URL"))
+        driver.maximize_window()
+        page_url = driver.current_url
+        assert os.getenv('BASE_URL') in page_url
+
+        time.sleep(5)
+
+    def test_cleanup(self):
+        driver.quit()
