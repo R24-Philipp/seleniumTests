201bfab09e34688b2bb0d56596281f4829bdcad1
R24-Philipp <pt@rueckwand24.com>
2023-02-07 13:27:31 +0100

    inital

--- test_run.py
+++ test_run.py
@@ -8,7 +8,9 @@ from selenium.webdriver.chrome.options import Options
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 class Testwebpage:
