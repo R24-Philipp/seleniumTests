44bd57e15f7062f2621bf98e1bb5e97e5d779215
R24-Philipp <pt@rueckwand24.com>
2023-02-07 14:44:33 +0100

    inital

--- test_run.py
+++ test_run.py
@@ -1,17 +1,10 @@
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(options=chrome_options)
+
+driver = webdriver.Chrome()
 
 class Testwebpage:
     load_dotenv()
