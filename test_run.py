032a2fbcddbf29087464090ad9e14c5ab6b616d3
R24-Philipp <pt@rueckwand24.com>
2023-02-07 13:33:22 +0100

    inital

--- test_run.py
+++ test_run.py
@@ -10,7 +10,6 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 class Testwebpage:
